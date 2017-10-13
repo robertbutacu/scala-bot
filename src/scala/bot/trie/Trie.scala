cde336b130867a117c37d3898cabe4b93acefafa
robertbutacu <robert.butacu@yahoo.com>
2017-10-13 22:34:52 +0300

    removed unnecessary case class Leaf

--- src/scala/bot/trie/Trie.scala
+++ src/scala/bot/trie/Trie.scala
@@ -3,9 +3,8 @@ package scala.bot.trie
 import scala.bot.handler.Attribute
 import scala.util.matching.Regex
 
-case class Leaf(replies: Set[String])
 
-case class Node(curr: (Regex, Option[Attribute]), children: Set[Node], leafs: Leaf) {
+case class Node(curr: (Regex, Option[Attribute]), children: Set[Node], replies: Set[String]) {
   def addValue(node: Node): Node =
-    Node(curr, children ++ Set(node), leafs)
+    Node(curr, children ++ Set(node), replies)
 }
--- src/scala/bot/trie/TrieOperations.scala
+++ src/scala/bot/trie/TrieOperations.scala
@@ -9,11 +9,11 @@ trait TrieOperations {
   def add(sentence: List[Word], replies: Set[String], trie: Node): Node = {
     def go(curr: Node, words: List[Word]): Node = {
       if (words.isEmpty)
-        Node(curr.curr, curr.children, Leaf(curr.leafs.replies ++ replies))
+        Node(curr.curr, curr.children, curr.replies ++ replies)
       else {
         curr.children.find(n  => isMatching(n.curr, words.head)) match {
           case None           => go(curr.addValue(createNode(words.head)), words)
-          case Some(next)     => Node(curr.curr, curr.children - next ++ Set(go(next, words.tail)), curr.leafs)
+          case Some(next)     => Node(curr.curr, curr.children - next ++ Set(go(next, words.tail)), curr.replies)
         }
       }
     }
@@ -22,12 +22,12 @@ trait TrieOperations {
   }
 
   def printTrie(trie: Node): Unit = {
-    print(trie.curr + "   ")
-    trie.leafs.replies.foreach(r => println("Leaf " + r))
+    println("Node " + trie.curr + "   ")
+    trie.replies.foreach(r => println("Leaf " + r))
     trie.children.foreach(t => printTrie(t))
   }
 
-  private def createNode(word: Word): Node = Node(word, Set[Node]().empty, Leaf(Set[String]().empty))
+  private def createNode(word: Word): Node = Node(word, Set[Node]().empty, Set[String]().empty)
 
   private def isMatching(node: Word, that: Word): Boolean =
     node._1.regex == that._1.regex && node._2 == that._2
