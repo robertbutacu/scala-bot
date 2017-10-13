f05b8a022482888fd77d8c6d3386204ae85dcf68
robertbutacu <robert.butacu@yahoo.com>
2017-10-13 12:22:21 +0300

    some part of add(), but will need more work

---
+++ src/scala/bot/trie/TrieOperations.scala
@@ -0,0 +1,26 @@
+package scala.bot.trie
+
+import scala.bot.handler.Attribute
+import scala.util.matching.Regex
+
+trait TrieOperations {
+  type Word = (Regex, Option[Attribute])
+
+  def add(sentence: List[Word], possibleReplies: List[String], trie: Node): Node = {
+    def go(curr: Node, remainingSentence: List[Word]): Node = {
+      if (remainingSentence.isEmpty)
+        curr
+      else {
+        curr.children.find(n => isMatching(n.current, sentence.head)) match {
+          case None        => go(Node(sentence.head, Set[Node]().empty, Set[Leaf]().empty), remainingSentence)
+          case Some(_) => curr
+        }
+      }
+    }
+
+    go(trie, sentence)
+  }
+
+  private def isMatching(node: Word, partOfSentence: Word): Boolean =
+    node._1 == partOfSentence._1 && node._2 == partOfSentence._2
+}
