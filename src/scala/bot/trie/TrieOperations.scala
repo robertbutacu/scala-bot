6eec340e5693cea696ddbb97a0e6ae1564bd6ed4
robertbutacu <robert.butacu@yahoo.com>
2017-10-14 16:22:51 +0300

    docs

--- src/scala/bot/trie/TrieOperations.scala
+++ src/scala/bot/trie/TrieOperations.scala
@@ -7,6 +7,14 @@ import scala.util.matching.Regex
 trait TrieOperations {
   type Word = (Regex, Option[Attribute])
 
+  /**
+    * For a current Trie, the algorithm returns another trie with the message added.
+    *
+    * @param words - list of words to be added into the trie
+    * @param replies - possible replies to the current message
+    * @param trie    - trie where the message will be stored
+    * @return
+    */
   final def add(words: List[Word], replies: (Option[String], Set[String]), trie: Trie): Trie = {
     def go(curr: Trie, words: List[Word]): Trie = {
       if (words.isEmpty) //went through all the list
@@ -25,6 +33,15 @@ trait TrieOperations {
     go(trie, words)
   }
 
+  /**
+    * The algorithm describes the search of a message in a trie, by parsing every word and matching it,
+    * thus returning a Set of possible replies depending on bot's previous replies.
+    *
+    * @param words - the sentence that is to be found, or not
+    * @param trie  - the trie in which it will be searched
+    * @return      - returns a Set of (previousMessageFromBot, Set[possible replies]),
+    *              from which another algorithm will pick the best choice.
+    */
   @tailrec
   final def search(words: List[Word], trie: Trie): Set[(Option[String], Set[String])] = {
     if (words.isEmpty)
@@ -44,18 +61,41 @@ trait TrieOperations {
     trie.children.foreach(t => printTrie(t))
   }
 
+  /**
+    *
+    * @param t - trie on which is operated
+    * @param replies - replies that are to be added
+    * @return - new leafs which also contain the new replies
+    *         There are 2 cases:
+    *             1. when they depend on a previous bot message ( or lack of) also stored:
+    *                 the replies are appended to the already existing replies.
+    *             2. when they aren't stored at all:
+    *                 they are registered as new replies with their attribute.
+    */
   private def addReplies(t: Trie, replies: (Option[String], Set[String])): Trie =
     t.replies.find(l => l._1 == replies._1) match {
       case None      => Trie(t.curr, t.children, t.replies ++ Set(replies))
       case Some(rep) => Trie(t.curr, t.children, t.replies -- Set(rep) ++ Set((rep._1, rep._2 ++ replies._2)))
     }
 
+  /**
+    *There are 4 cases:
+    *   1. for when t is not a regex ( plain message):
+    *       a. word is also a message => check for equality
+    *       b. word is an attribute        => they don't match
+    *   2. t is a regex:
+    *       a. word is not an attribute => they don't match
+    *       b. word is an attribute     => check if it matches the regex
+    * @param t - trie word
+    * @param w - message word
+    * @return
+    */
   private def isMatching(t: Trie, w: Word): Boolean = {
     t.curr match {
       case (tRex, None)        =>
         w match {
-          case (wRex, None) => tRex.regex == wRex.regex //both are not attributes
-          case (_, Some(_)) => false //word is an attribute, but the node isnt
+          case (wRex, None) => tRex.regex == wRex.regex //neither are attributes
+          case (_, Some(_)) => false //word is an attribute, but the node isn't
         }
       case (tRex, Some(tAttr)) =>
         w match {
