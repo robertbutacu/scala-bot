e870a7e757cdb475d1bed010ecd4c6039941aab0
robertbutacu <robert.butacu@yahoo.com>
2017-10-15 11:29:15 +0300

    implemented learn

--- src/scala/bot/learn/Learner.scala
+++ src/scala/bot/learn/Learner.scala
@@ -5,20 +5,15 @@ import scala.bot.handler.Attribute
 import scala.bot.trie.{Trie, TrieOperations}
 import scala.util.matching.Regex
 
-trait Learner extends TrieOperations{
+trait Learner extends TrieOperations {
   type Templates = Map[((Option[String]), List[Either[String, (Regex, Attribute)]]), Responses]
   type Responses = Set[String]
 
-  def learn(old: Trie , acquired: Templates): Trie ={
-    /*def learnMessage(trie: Trie, message: List[Word], replies: (Option[String], Set[String])): Trie =
-      add(message, replies, trie)
-
+  def learn(old: Trie, acquired: Templates): Trie =
     acquired.toList match {
-      case h::tail => add(toWords(add(h._1._2)), (h._1._1, h._2), old)
-    }*/
-    Trie()
-
-  }
+      case h :: tail => learn(add(toWords(h._1._2), (h._1._1, h._2), old), tail.toMap)
+      case Nil       => old
+    }
 
 
   def learn(old: Templates, acquired: List[Templates]): Trie = {
@@ -41,9 +36,10 @@ trait Learner extends TrieOperations{
     *         or a regex with an attribute
     */
   def toWords(message: List[Either[String, (Regex, Attribute)]]): List[Word] =
-    message flatMap{w =>
-    w match {
-      case Left(words) => words.split(' ').toList.map(w => (w.r, None))
-      case Right((r, attr)) => List((r, Some(attr)))
-    }} filterNot ( _._1.toString() == "")
+    message flatMap { w =>
+      w match {
+        case Left(words)      => words.split(' ').toList.map(w => (w.r, None))
+        case Right((r, attr)) => List((r, Some(attr)))
+      }
+    } filterNot (_._1.toString() == "")
 }
