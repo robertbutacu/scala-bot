db11ab014f28bcccf2ca24123dc5c1201af59e66
robertbutacu <robert.butacu@yahoo.com>
2017-11-10 17:54:34 +0200

    refactored RepliesLearner so it works with Reply

--- src/scala/bot/learn/RepliesLearner.scala
+++ src/scala/bot/learn/RepliesLearner.scala
@@ -2,50 +2,33 @@ package scala.bot.learn
 
 import scala.annotation.tailrec
 import scala.bot.trie.TrieOperations._
-import scala.bot.trie.{Attribute, Trie}
-import scala.util.matching.Regex
+import scala.bot.trie.{PartOfMessage, Reply, Trie}
 
 object RepliesLearner {
-  /**
-    * Templates are composed of a Map of :
-    *   - a tuple containing:
-    *     1. Option[() => Set[String] ] => an optional previous bot message represented as a function that returns
-    *                                       a set of strings ( so that everything is dynamic ).
-    *     2. List[Either[String, (Regex, Attribute)] ] => the message of the client itself, composed of a list
-    *                                       mixed with both parts of a sentence ( " I am " ), but
-    *                                       regexes and attributes also ( "[A-z][a-z]*", NameAttr ).
-    *   - a response to a particular message:
-    *     1. Responses => a set of functions returning a set of strings.
-    *
-    */
-
-  type Templates = Map[(Option[() => Set[String]], List[Either[String, (Regex, Attribute)]]), Responses]
-
   type Responses = Set[() => Set[String]]
 
   /**
-    * @param old - previous trie to which new templates are to be added
+    * @param trie     - previous trie to which new templates are to be added
     * @param acquired - templates to be added
     * @return - a new trie with the acquired templates in memory
     */
-  @tailrec
-  final def learn(old: Trie, acquired: Templates): Trie =
-    acquired.toList match {
-      case h :: tail => learn(add(toWords(h._1._2), (h._1._1, h._2), old), tail.toMap)
-      case Nil       => old
-    }
+  final def learn(trie: Trie, acquired: Reply): Trie =
+    add(toWords(acquired.humanMessage.message),
+      (acquired.humanMessage.previousBotReply, acquired.botReplies),
+      trie)
+
 
   /**
-    * @param old - previous trie to which new templates are to be added
+    * @param old      - previous trie to which new templates are to be added
     * @param acquired - a list of templates to be added
     * @return - a new trie with the list of acquired templates in memory
     */
-  def learn(old: Trie, acquired: List[Templates]): Trie = {
+  def learn(old: Trie, acquired: List[Reply]): Trie = {
     @tailrec
-    def startLearning(curr: Trie, toBeLearned: List[Templates]): Trie = {
+    def startLearning(curr: Trie, toBeLearned: List[Reply]): Trie = {
       toBeLearned match {
-        case Nil     => curr
-        case h::tail => startLearning(learn(curr, h), tail)
+        case Nil => curr
+        case h :: tail => startLearning(learn(curr, h), tail)
       }
     }
 
@@ -61,10 +44,10 @@ object RepliesLearner {
     * @return a list of words that could be either a string with no attr set,
     *         or a regex with an attribute
     */
-  def toWords(message: List[Either[String, (Regex, Attribute)]]): List[Word] =
+  def toWords(message: List[PartOfMessage]): List[Word] =
     message flatMap { w =>
-      w match {
-        case Left(words)      => words.split(' ').toList.map(w => (w.r, None))
+      w.part match {
+        case Left(words) => words.split(' ').toList.map(w => (w.r, None))
         case Right((r, characteristic)) => List((r, Some(characteristic)))
       }
     } filterNot (_._1.toString() == "")
