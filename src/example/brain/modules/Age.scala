2654f141f539d227418b4eceebefae722d3739b1
robertbutacu <robert.butacu@yahoo.com>
2017-11-10 18:09:15 +0200

    Age trait behaves as intended

--- src/example/brain/modules/Age.scala
+++ src/example/brain/modules/Age.scala
@@ -2,17 +2,31 @@ package example.brain.modules
 
 import example.brain.BrainFunctions
 
-import scala.bot.learn.RepliesLearner.{Responses, Templates}
-import scala.bot.trie.Attribute
+import scala.bot.learn.RepliesLearner.Responses
+import scala.bot.trie.{Attribute, HumanMessage, PartOfMessage, Reply}
 import scala.util.matching.Regex
 
+/*
+case class Reply(humanMessage: HumanMessage,
+                 botReplies: Responses)
+
+case class HumanMessage(previousBotReply: Option[() => Set[String]],
+                        message: List[PartOfMessage])
+
+case class PartOfMessage(part: Either[String, (Regex, Attribute)])
+
+ */
+
 trait Age extends BrainFunctions with Attributes{
-  val ages: Templates = Map[(Option[() => Set[String]], List[Either[String, (Regex, Attribute)]]), Responses](
-    (None, List(Left("Im "),
-      Right("[0-9]+".r, age),
-      Left(" years old"))) -> Set(ageReply _),
-    (None, List(Left("Im passionate about"),
-      Right("[a-zA-Z]+".r, passion))) -> Set(passionReply _),
-    (Some(passionReply _), List(Left("What am i passionate about"))) -> Set(passionReplies _)
+  val ages: List[Reply] = List(
+    Reply(HumanMessage(None, List(PartOfMessage(Left("Im ")),
+      PartOfMessage(Right("[0-9]+".r, age)),
+      PartOfMessage(Left(" years old")))), Set(ageReply _)),
+    Reply(HumanMessage(None, List(PartOfMessage(Left("Im passionate about")),
+      PartOfMessage(Right("[a-zA-Z]+".r, passion)))),
+      Set(passionReply _)),
+    Reply(HumanMessage(Some(passionReply _),
+      List(PartOfMessage(Left("What am i passionate about")))),
+      Set(passionReplies _))
   )
 }
