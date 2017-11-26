eb3603625c194861a6bc313141fe1d36ea3b0b79
robertbutacu <robert.butacu@yahoo.com>
2017-11-26 13:46:50 +0200

    implemented forget(), add()

--- src/scala/bot/memory/BotMemory.scala
+++ src/scala/bot/memory/BotMemory.scala
@@ -6,10 +6,12 @@ trait BotMemory {
   def persist(people: List[Person]): Unit = ???
 
   def forget(people: List[Map[Attribute, String]],
-             person: Map[Attribute, String]): List[Map[Attribute, String]] = ???
+             person: Map[Attribute, String]): List[Map[Attribute, String]] =
+    people.filterNot(_ == person)
 
   def add(people: List[Map[Attribute, String]],
-          person: Map[Attribute, String]): List[Map[Attribute, String]] = ???
+          person: Map[Attribute, String]): List[Map[Attribute, String]] =
+    people :+ person
 
   def remember(filename: String): List[Person] = ???
 
