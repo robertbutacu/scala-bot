a772a393bbc893179647cc93a4f27a11544b43d0
robertbutacu <robert.butacu@yahoo.com>
2017-11-26 13:44:46 +0200

    scaffolding

--- src/scala/bot/memory/BotMemory.scala
+++ src/scala/bot/memory/BotMemory.scala
@@ -3,9 +3,19 @@ package scala.bot.memory
 import scala.bot.trie.Attribute
 
 trait BotMemory {
+  def persist(people: List[Person]): Unit = ???
+
+  def forget(people: List[Map[Attribute, String]],
+             person: Map[Attribute, String]): List[Map[Attribute, String]] = ???
+
+  def add(people: List[Map[Attribute, String]],
+          person: Map[Attribute, String]): List[Map[Attribute, String]] = ???
+
   def remember(filename: String): List[Person] = ???
 
   def translate(people: List[Person]): List[Map[Attribute, String]] = ???
 
-  def tryMatch(people: List[Map[Attribute, String]], minThreshold : Int): List[Map[Attribute, String]] = ???
+  def tryMatch(people: List[Map[Attribute, String]],
+               person: Map[Attribute, String],
+               minThreshold: Int): List[Map[Attribute, String]] = ???
 }
