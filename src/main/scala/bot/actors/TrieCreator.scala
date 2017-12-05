7b5c9a2acbde1e4e86697670e5565989bd5a637f
robertbutacu <robert.butacu@yahoo.com>
2017-12-05 22:43:28 +0200

    receive() for TrieCreator

--- src/main/scala/bot/actors/TrieCreator.scala
+++ src/main/scala/bot/actors/TrieCreator.scala
@@ -1,9 +1,9 @@
 package bot.actors
 
 import akka.actor.{Actor, Props}
-import bot.actors.TrieCreator.Print
+import bot.actors.TrieCreator._
 import bot.trie.Trie
-import bot.trie.TrieOperations.Word
+import bot.trie.TrieOperations._
 
 object TrieCreator {
   def props() = Props(new TrieCreator)
@@ -21,12 +21,23 @@ object TrieCreator {
   case class TrieResponse(trie: Trie)
 
 
-  case object Print
+  case class CreateTrie(message: List[Word],
+                        replies: (Option[() => Set[String]], Set[() => Set[String]]))
+
+  case class Print(trie: Trie)
+
+  case class SearchReturnMessage(response: SearchResponse)
 
 }
 
 class TrieCreator() extends Actor {
   override def receive: Actor.Receive = {
-    case Print => println("asdfasd")
+    case Print(trie: Trie) => printTrie(trie)
+
+    case CreateTrie(msg, replies) => sender() ! TrieResponse(add(msg, replies, Trie()))
+
+    case Search(msg, trie) => sender() ! SearchReturnMessage(search(msg, trie))
+
+    case Add(msg, replies, trie) => sender() ! TrieResponse(add(msg, replies, trie))
   }
 }
