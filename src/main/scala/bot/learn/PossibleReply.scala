84080ace048dc1cdd157b464d304c361dfa901e0
robertbutacu <robert.butacu@yahoo.com>
2017-12-10 11:09:15 +0200

    PossibleReply is a case class

--- src/main/scala/bot/learn/PossibleReply.scala
+++ src/main/scala/bot/learn/PossibleReply.scala
@@ -1,11 +1,5 @@
 package bot.learn
 
-class PossibleReply(val previousBotMessage: Option[() => Set[String]],
-                    val possibleReply: Set[() => Set[String]]) {
-}
-
-object PossibleReply {
-  def apply(previousBotMessage: Option[() => Set[String]],
-            possibleReply: Set[() => Set[String]]): PossibleReply =
-    new PossibleReply(previousBotMessage, possibleReply)
+case class PossibleReply(previousBotMessage: Option[() => Set[String]],
+                         possibleReply: Set[() => Set[String]]) {
 }
