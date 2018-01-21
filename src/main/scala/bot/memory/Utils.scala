0829a4fbb01a38a312d06b13a63018539be6c3c7
robertbutacu <robert.butacu@yahoo.com>
2018-01-21 18:59:09 +0200

    wordsInSameContext now returns the Option Monad which can be easily used in a for comprehension

--- src/main/scala/bot/memory/Utils.scala
+++ src/main/scala/bot/memory/Utils.scala
@@ -2,6 +2,8 @@ package bot.memory
 
 import bot.memory.definition.{Definition, NodeWord, Synonym, Word}
 
+import scala.util.matching.Regex
+
 protected[memory] object Utils {
 
   def findReplacements(sentence: List[NodeWord], word: NodeWord, dictionary: Set[Definition]): List[Word] = {
@@ -9,26 +11,34 @@ protected[memory] object Utils {
       def unapply(): Word = this.synonym.definition
     }
 
-    def wordsInSameContext(sentence: List[NodeWord], contextWords: Set[Word]): Int = {
-      for {
-        word <- sentence
-        contextWord <- contextWords
-        otherAcceptableForms <- contextWord.otherAcceptableForms
-      } yield word.word.toString() == contextWord.word || word.toString == otherAcceptableForms.word
-    }.size
+    def wordsInSameContext(sentence: List[NodeWord], contextWords: Set[Word]): Option[Int] = {
+      implicit def nodeWordToString(n: NodeWord): String = n.word.toString()
+
+      def isMatch(nw: String, toMatch: Word): Boolean = {
+        toMatch.word == nw || toMatch.otherAcceptableForms.exists(isMatch(nw, _))
+      }
+
+      val count = sentence count {
+        w => contextWords exists (isMatch(w, _))
+      }
+
+      if (count > 0) Some(count)
+      else None
+    }
 
     val definitionM = dictionary find { p => p.word.word == word.word.toString() }
 
     //having the definition found out, try to zip each synonym with the number of words found in that context
-    val contextsCount =     for {
+    val contextsCount = for {
       definition <- definitionM.toList
       synonym <- definition.synonyms
-      if wordsInSameContext(sentence, synonym.contextWords) > 0
-    } yield ContextMatchCount(synonym, wordsInSameContext(sentence, synonym.contextWords))
+      count <- wordsInSameContext(sentence, synonym.contextWords).toList
+      if count > 0
+    } yield ContextMatchCount(synonym, count)
 
     //max by apparitions
     val maxMatch = contextsCount.maxBy(_.count)
 
-    contextsCount filter { _.count == maxMatch} map { _.unapply()}
+    contextsCount filter (_.count == maxMatch) map (_.unapply())
   }
 }
