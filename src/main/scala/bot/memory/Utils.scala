f1dd0dbc4893836e9484b6ee56e1e147b72bc794
robertbutacu <robert.butacu@yahoo.com>
2018-01-21 18:47:31 +0200

    works and looks decent now

--- src/main/scala/bot/memory/Utils.scala
+++ src/main/scala/bot/memory/Utils.scala
@@ -5,7 +5,9 @@ import bot.memory.definition.{Definition, NodeWord, Synonym, Word}
 protected[memory] object Utils {
 
   def findReplacements(sentence: List[NodeWord], word: NodeWord, dictionary: Set[Definition]): List[Word] = {
-    case class ContextMatchCount(synonym: Synonym, count: Int)
+    case class ContextMatchCount(synonym: Synonym, count: Int) {
+      def unapply(): Word = this.synonym.definition
+    }
 
     def wordsInSameContext(sentence: List[NodeWord], contextWords: Set[Word]): Int = {
       for {
@@ -18,23 +20,15 @@ protected[memory] object Utils {
     val definitionM = dictionary find { p => p.word.word == word.word.toString() }
 
     //having the definition found out, try to zip each synonym with the number of words found in that context
-
-    val contextCount = definitionM map { d =>
-      d.synonyms
-        .filter(s => wordsInSameContext(sentence, s.contextWords) > 0)
-        .map(s => ContextMatchCount(s, wordsInSameContext(sentence, s.contextWords)))
-    }
-    /*for {
-      definition <- definitionM
+    val contextsCount =     for {
+      definition <- definitionM.toList
       synonym <- definition.synonyms
       if wordsInSameContext(sentence, synonym.contextWords) > 0
     } yield ContextMatchCount(synonym, wordsInSameContext(sentence, synonym.contextWords))
-    */
-    //max by apparitions
-    val maxMatch = contextCount.map(c => c.maxBy(_.count).count)
 
-    contextCount.map(c => c.filter(cc => maxMatch.getOrElse(-1) == cc.count))
-      .map(cc => cc.map(_.synonym.definition))
-      .getOrElse(List.empty).toList
+    //max by apparitions
+    val maxMatch = contextsCount.maxBy(_.count)
+
+    contextsCount filter { _.count == maxMatch} map { _.unapply()}
   }
 }
