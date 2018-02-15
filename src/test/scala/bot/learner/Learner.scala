93ade06d4135eb7c011958791dd225b402864b50
robertbutacu <robert.butacu@yahoo.com>
2018-02-15 21:26:53 +0200

    setting up Learner ( ish )

--- src/test/scala/bot/learner/Learner.scala
+++ src/test/scala/bot/learner/Learner.scala
@@ -1,7 +1,11 @@
 package bot.learner
 
+import bot.handler.MessageHandler
+import example.brain.modules.Attributes
 import org.scalatest.FlatSpec
 
-class Learner extends FlatSpec {
-
+class Learner extends FlatSpec with Attributes with MessageHandler {
+  def ageReply(): Set[String] = {
+    Set("Ok", "Test")
+  }
 }
