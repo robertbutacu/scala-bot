e0092ab955259a4cfda28f6889e51c2ac7d4a98b
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-01-31 14:51:18 +0000

    indentation

--- src/main/scala/bot/memory/part/of/speech/PartOfSpeech.scala
+++ src/main/scala/bot/memory/part/of/speech/PartOfSpeech.scala
@@ -2,13 +2,13 @@ package bot.memory.part.of.speech
 
 sealed trait PartOfSpeech
 
-case class Noun(grammaticalNumber: GrammaticalNumber = Singular) extends PartOfSpeech
+case class Noun(grammaticalNumber: GrammaticalNumber = Singular)      extends PartOfSpeech
 case class Adjective(grammaticalNumber: GrammaticalNumber = Singular) extends PartOfSpeech
-case class Pronoun(grammaticalNumber: GrammaticalNumber = Singular) extends PartOfSpeech
-case object Adverb extends PartOfSpeech
+case class Pronoun(grammaticalNumber: GrammaticalNumber = Singular)   extends PartOfSpeech
+case object Adverb                          extends PartOfSpeech
 case class Verb(tense: VerbTense = Present) extends PartOfSpeech
-case object Preposition extends PartOfSpeech
-case object Conjunction extends PartOfSpeech
+case object Preposition  extends PartOfSpeech
+case object Conjunction  extends PartOfSpeech
 case object Interjection extends PartOfSpeech
-case object Irrelevant extends PartOfSpeech
+case object Irrelevant   extends PartOfSpeech
 
