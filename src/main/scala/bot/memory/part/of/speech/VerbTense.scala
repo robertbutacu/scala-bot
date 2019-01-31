19151f8ca20ce5fe646124bcc07c73517e9e5c1b
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-01-31 14:51:26 +0000

    indentation

--- src/main/scala/bot/memory/part/of/speech/VerbTense.scala
+++ src/main/scala/bot/memory/part/of/speech/VerbTense.scala
@@ -2,6 +2,6 @@ package bot.memory.part.of.speech
 
 sealed trait VerbTense
 
-case object Past extends VerbTense
+case object Past    extends VerbTense
 case object Present extends VerbTense
-case object Future extends VerbTense
+case object Future  extends VerbTense
