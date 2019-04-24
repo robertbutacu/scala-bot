cc3231c8a972756446b85eb7479ba967ab3ee612
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-04-24 18:29:53 +0100

    indentation

--- src/main/scala/example/brain/definitions/Definitions.scala
+++ src/main/scala/example/brain/definitions/Definitions.scala
@@ -10,24 +10,24 @@ object Definitions {
   def get(): Set[Definition] = {
 
     implicit def convertString(s: String): Regex = s.r
-    implicit def convertDefinitionToWord(d: Definition) : Word = d.word
-    implicit def convertDefinition(d: Definition): Synonym = Synonym(d)
-    implicit def convertToWord(d: String): Word = Word(d)
+    implicit def convertDefinitionToWord(d: Definition) : Word    = d.word
+    implicit def convertDefinition(d: Definition):        Synonym = Synonym(d)
+    implicit def convertToWord(d: String):                Word    = Word(d)
 
     val underage: Definition = Definition(Word("underage"))
-    val minor: Definition = Definition(Word("minor"))
+    val minor:    Definition = Definition(Word("minor"))
 
-    val old: Definition = Definition(Word("age"))
+    val old:    Definition = Definition(Word("age"))
     val ageOld: Definition = Definition(Word("age-old"), Set(old))
 
     val passionate: Definition = Definition(Word("passionate"))
-    val ardent: Definition = Definition(Word("ardent"))
-    val keen: Definition = Definition(Word("keen"))
+    val ardent:     Definition = Definition(Word("ardent"))
+    val keen:       Definition = Definition(Word("keen"))
 
     val greetings: Definition = Definition(Word("greetings"))
-    val hi: Definition = Definition(Word("hi"))
-    val hello: Definition = Definition(Word("hello"))
-    val whatsup: Definition = Definition(Word("hello"))
+    val hi:        Definition = Definition(Word("hi"))
+    val hello:     Definition = Definition(Word("hello"))
+    val whatsup:   Definition = Definition(Word("hello"))
 
     Set(Definition.addDefinitions(underage, Set(minor)),
       Definition.addDefinitions(minor, Set(underage)),
