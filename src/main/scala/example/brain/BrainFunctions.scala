b992e3737a66069784367cc7a0877ca84a9724ab
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-04-24 18:25:18 +0100

    indentation

--- src/main/scala/example/brain/BrainFunctions.scala
+++ src/main/scala/example/brain/BrainFunctions.scala
@@ -6,7 +6,7 @@ import example.brain.modules.Attributes
 trait BrainFunctions extends MessageHandler with Attributes {
   def ageReply(): Set[String] = {
     getAttribute(age) match {
-      case None => Set("Unknown age", "You havent told me your age")
+      case None    => Set("Unknown age", "You havent told me your age")
       case Some(a) => provideReplies(a.toInt)
     }
   }
@@ -17,20 +17,20 @@ trait BrainFunctions extends MessageHandler with Attributes {
       case _ if age > 24 => Set("you're an adult", "children?")
       case _ if age > 18 => Set("programming", "is", "fun")
       case _ if age > 0 && age < 18 => Set("Underage", "Minor")
-      case _ if age < 0 => Set(s"""It appears your age is $age . What a lie. Tell me your real age please.""")
-      case _ => Set("got me there", "lost")
+      case _ if age < 0  => Set(s"""It appears your age is $age . What a lie. Tell me your real age please.""")
+      case _             => Set("got me there", "lost")
     }
 
   def passionReply(): Set[String] = {
     getAttribute(passion) match {
-      case None => Set("No passions")
+      case None      => Set("No passions")
       case Some(pas) => Set(s"""Passionate about $pas""")
     }
   }
 
   def passionReplies(): Set[String] =
     getAttribute(passion) match {
-      case None => Set("You're not passionate about anything")
+      case None    => Set("You're not passionate about anything")
       case Some(p) => Set(s"""You're passionate about $p""")
     }
 }
