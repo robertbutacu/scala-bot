473e96c0ec2da67f51968c5fe8e480c73c36a86e
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-05-04 17:01:32 +0100

    removed the whole clustering module at least for now

--- src/main/scala/example/brain/Manager.scala
+++ src/main/scala/example/brain/Manager.scala
@@ -1,6 +1,5 @@
 package example.brain
 
-import bot.clustering.Clusterize
 import bot.learn.RepliesLearner._
 import bot.memory.Trie
 import bot.memory.definition.NodeSimpleWord
@@ -8,6 +7,5 @@ import example.brain.definitions.Definitions
 import example.brain.modules.MasterModule
 
 trait Manager extends MasterModule {
-  val masterBrain:                 Trie       = learn(Trie(NodeSimpleWord("".r)), List(jobs, ages, greetings).flatten, Definitions.get())
-  lazy val clusterizedMasterBrain: List[Trie] = Clusterize.apply(List(jobs, ages, greetings).flatten, 2, Definitions.get())
+  val masterBrain: Trie = learn(Trie(NodeSimpleWord("".r)), List(jobs, ages, greetings).flatten, Definitions.get())
 }
